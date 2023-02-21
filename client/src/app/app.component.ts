0e3c369406c786fef97dc862d2724eb80f8b227e
go_projects_og <go.TIIT.ogle@gmail.com>
2023-02-21 10:38:55 +0300

    19

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,6 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  now: Date = new Date();
+  arr = [1, 2, 3, 4, 5, 6, 7];
+
+  objs = [
+    {
+      title: 'post1',
+      author: 'Tigran',
+      comments: [
+        {
+          name: 'Max',
+          text: 'lorem1',
+        },
+        {
+          name: 'Max',
+          text: 'lorem1',
+        },
+        {
+          name: 'Max',
+          text: 'lorem1',
+        },
+      ],
+    },
+    {
+      title: 'post2',
+      author: 'Tigran',
+      comments: [
+        {
+          name: 'Max',
+          text: 'lorem1',
+        },
+        {
+          name: 'Max',
+          text: 'lorem1',
+        },
+        {
+          name: 'Max',
+          text: 'lorem1',
+        },
+      ],
+    },
+  ];
   // backgroundToggle = false;
   toggle: any = false;
 }
